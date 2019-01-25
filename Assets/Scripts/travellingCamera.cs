68a65df0129cb24b107fc3f511282fc329320d53
localbea <localbea@D37-02-R.bellecour.int>
2019-01-25 23:36:42 +0100

    Leo push

---
+++ Assets/Scripts/travellingCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class travellingCamera : MonoBehaviour
+{
+    GameObject target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.Find("Ethan");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+
+        transform.position = targetPosition;
+    }
+}
