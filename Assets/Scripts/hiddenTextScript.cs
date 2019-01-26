5586199b7c2cf64e17e1e0eea3fd8667e5728d60
localbea <localbea@D37-01-R.bellecour.int>
2019-01-26 01:08:01 +0100

    hiddenTextScript

---
+++ Assets/Scripts/hiddenTextScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hiddenTextScript : MonoBehaviour {
+
+    public GameObject m_player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(transform.position, m_player.transform.position) < 4) {
+            TextMesh mesh = GetComponent(typeof(TextMesh)) as TextMesh;
+
+            if (mesh) {
+                mesh.color = new Color(mesh.color.r, mesh.color.g, mesh.color.b, 255);
+            }
+        } else {
+            TextMesh mesh = GetComponent(typeof(TextMesh)) as TextMesh;
+
+            if (mesh) {
+                mesh.color = new Color(mesh.color.r, mesh.color.g, mesh.color.b, 0);
+            }
+        }
+    }
+}
